4c8f0d69464e5de4e66dc85c03e0def3529577a2
powerson <pauer@email.cz>
2014-07-22 18:04:41 +0200

    1.1 #INT-2 for me fixed work 5m

--- Interest/Interest.cs
+++ Interest/Interest.cs
@@ -4,58 +4,59 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using movement;
 
 namespace Interest
 {
-    class Movement
-    {
-        public string type;
-        public float value;
-        public DateTime when;
+    //class Movement
+    //{
+    //    public string type;
+    //    public float value;
+    //    public DateTime when;
 
-        public int pointer(Movement[] myData, int lines)
-        {
-            int i = 0;
-            int j = 0;
-            for (i = 0; i < lines; i++)
-            {
-                if (myData[i].type == "B" && myData[i].when > myData[j].when)
-                {
-                    j = i;
-                }
-            }
-            return j;
-        }
+    //    public int pointer(Movement[] myData, int lines)
+    //    {
+    //        int i = 0;
+    //        int j = 0;
+    //        for (i = 0; i < lines; i++)
+    //        {
+    //            if (myData[i].type == "B" && myData[i].when > myData[j].when)
+    //            {
+    //                j = i;
+    //            }
+    //        }
+    //        return j;
+    //    }
 
-        public float result(Movement[] myData, int lines)
-        {
-            float result = 0.0F;
-            float sum = 0.0F;
-            float cumulate = 0.0F;
-            int diff;
-            int i = 0;
-            int j = 0;
+    //    public float result(Movement[] myData, int lines)
+    //    {
+    //        float result = 0.0F;
+    //        float sum = 0.0F;
+    //        float cumulate = 0.0F;
+    //        int diff;
+    //        int i = 0;
+    //        int j = 0;
 
-            for (i = 0; i < lines; i++)
-            {
-                if (myData[i].type == "B" && myData[i].when > myData[j].when)
-                {
-                    j = i;
-                }
-            }
-            for (i = 0; i < lines; i++)
-            {
-                if (myData[i].type == "D")
-                {
-                    diff = (myData[j].when - myData[i].when).Days;
-                    sum = sum + myData[i].value;
-                    cumulate = cumulate + diff * myData[i].value;
-                }
-            }
-            result = (myData[j].value - sum) * 365.0F / cumulate;
-            return result;
-        }
-    }
+    //        for (i = 0; i < lines; i++)
+    //        {
+    //            if (myData[i].type == "B" && myData[i].when > myData[j].when)
+    //            {
+    //                j = i;
+    //            }
+    //        }
+    //        for (i = 0; i < lines; i++)
+    //        {
+    //            if (myData[i].type == "D")
+    //            {
+    //                diff = (myData[j].when - myData[i].when).Days;
+    //                sum = sum + myData[i].value;
+    //                cumulate = cumulate + diff * myData[i].value;
+    //            }
+    //        }
+    //        result = (myData[j].value - sum) * 365.0F / cumulate;
+    //        return result;
+    //    }
+    //}
 
     class Program
     {
---
+++ Movement/Movement.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace movement
+{
+    public class Movement
+    {
+        public string type;
+        public float value;
+        public DateTime when;
+
+        public int pointer(Movement[] myData, int lines)
+        {
+            int i = 0;
+            int j = 0;
+            for (i = 0; i < lines; i++)
+            {
+                if (myData[i].type == "B" && myData[i].when > myData[j].when)
+                {
+                    j = i;
+                }
+            }
+            return j;
+        }
+
+        public float result(Movement[] myData, int lines)
+        {
+            float result = 0.0F;
+            float sum = 0.0F;
+            float cumulate = 0.0F;
+            int diff;
+            int i = 0;
+            int j = 0;
+            for (i = 0; i < lines; i++)
+            {
+                if (myData[i].type == "B" && myData[i].when > myData[j].when)
+                {
+                    j = i;
+                }
+            }
+            for (i = 0; i < lines; i++)
+            {
+                if (myData[i].type == "D")
+                {
+                    diff = (myData[j].when - myData[i].when).Days;
+                    sum = sum + myData[i].value;
+                    cumulate = cumulate + diff * myData[i].value;
+                }
+            }
+            result = (myData[j].value - sum) * 365.0F / cumulate;
+            return result;
+        }
+    }
+}
