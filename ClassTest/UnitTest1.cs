b98221875ba271c89200a1c1370641966d44a7a9
powerson <pauer@email.cz>
2014-07-23 09:22:34 +0200

    1.1.1 unit test added

---
+++ ClassTest/UnitTest1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using movement;
+
+namespace TestInterest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Movement[] TestMovement = new Movement[2];
+
+            TestMovement[0] = new Movement();
+            TestMovement[0].type = "D";
+            TestMovement[0].value = 100.0F;
+            TestMovement[0].when = DateTime.Today.AddDays(-365);
+
+            TestMovement[1] = new Movement();
+            TestMovement[1].type = "B";
+            TestMovement[1].value = 100.0F;
+            TestMovement[1].when = DateTime.Today;
+
+            Movement myResult = new Movement(); //???
+            float result = myResult.result(TestMovement, TestMovement.Length);
+
+            Assert.AreEqual(0, result, "Result is different from expected.");
+        }
+    }
+}
+
--- Interest/Interest.cs
+++ Interest/Interest.cs
@@ -8,56 +8,6 @@ using movement;
 
 namespace Interest
 {
-    //class Movement
-    //{
-    //    public string type;
-    //    public float value;
-    //    public DateTime when;
-
-    //    public int pointer(Movement[] myData, int lines)
-    //    {
-    //        int i = 0;
-    //        int j = 0;
-    //        for (i = 0; i < lines; i++)
-    //        {
-    //            if (myData[i].type == "B" && myData[i].when > myData[j].when)
-    //            {
-    //                j = i;
-    //            }
-    //        }
-    //        return j;
-    //    }
-
-    //    public float result(Movement[] myData, int lines)
-    //    {
-    //        float result = 0.0F;
-    //        float sum = 0.0F;
-    //        float cumulate = 0.0F;
-    //        int diff;
-    //        int i = 0;
-    //        int j = 0;
-
-    //        for (i = 0; i < lines; i++)
-    //        {
-    //            if (myData[i].type == "B" && myData[i].when > myData[j].when)
-    //            {
-    //                j = i;
-    //            }
-    //        }
-    //        for (i = 0; i < lines; i++)
-    //        {
-    //            if (myData[i].type == "D")
-    //            {
-    //                diff = (myData[j].when - myData[i].when).Days;
-    //                sum = sum + myData[i].value;
-    //                cumulate = cumulate + diff * myData[i].value;
-    //            }
-    //        }
-    //        result = (myData[j].value - sum) * 365.0F / cumulate;
-    //        return result;
-    //    }
-    //}
-
     class Program
     {
         static void Main(string[] args)
@@ -94,10 +44,10 @@ namespace Interest
             {
                 if (myMovement[i].type == "D")
                 {
-                    Console.Write(myMovement[i].type + " " + myMovement[i].value + " on " + myMovement[i].when.ToShortDateString());
-                    Console.WriteLine();
+                    Console.WriteLine(myMovement[i].type + " " + myMovement[i].value + " on " + myMovement[i].when.ToShortDateString());
                 }
             }
+            Console.WriteLine(myMovement[j].type + " " + myMovement[j].value + " on " + myMovement[j].when.ToShortDateString());
             Console.WriteLine("So far it's " + result * 100.0F + "% interest this year");
             Console.WriteLine();
 
