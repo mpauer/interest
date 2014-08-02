73eb0ea5b430a9da8e59b05b98090e313eb6e2c7
powerson <pauer@email.cz>
2014-08-02 18:45:18 +0200

    reworked and more accounts

--- Interest/Interest.cs
+++ Interest/Interest.cs
@@ -13,49 +13,87 @@ namespace Interest
         static void Main(string[] args)
         {
             string[] myParts;
-            List<string>   myFond = new List<string>();
-            List<char>     myType = new List<char>();
+            List<int>    myFoNo = new List<int>();
+            List<string> myFond = new List<string>();
+            List<char> myType = new List<char>();
             List<DateTime> myDate = new List<DateTime>();
             List<float>    myAmmo = new List<float>();
-            int diff;
-            float sum = 0.0f;
-            float cumulate = 0.0f;
-            float result = 0.0f;
-
+            int i = 0;
+            int myIndex;
+            int myIndexB;
+            int myIndexD;
+            int myDiff;
+            
             string[] myLines = File.ReadAllLines("input.txt");
-            foreach (string lineX in myLines)
+            foreach (string myLineX in myLines)
             {
-                if (lineX == "") continue;
+                if (myLineX == "") continue;
                 
-                myParts = lineX.Split('\t');
+                myParts = myLineX.Split('\t');
                 if (myParts.Length != 4) continue;
+
+                myIndex = myFond.IndexOf(myParts[0]);
+                if (myIndex < 0)
+                {
+                    myFoNo.Add(i);
+                    i++;
+                }
+                else
+                {
+                    myFoNo.Add(myFoNo[myIndex]);
+                }
 
                 myFond.Add(myParts[0]);
                 myType.Add(char.Parse(myParts[1]));
                 myDate.Add(DateTime.Parse(myParts[2]));
                 myAmmo.Add(float.Parse(myParts[3]));
+            }
 
-            }
-            int myIndex = myType.IndexOf('B');
-            if (myIndex < 0)
+            float mySum;
+            float myCumulate;
+            float myResult;
+
+            int k;
+            for (int j = 0; j < i; j++)
             {
-                Console.WriteLine("There is no balance in the file!");
-            }
-            else
-            {
-                for (int i = 0; i < myType.Count; i++)
+                mySum = 0.0f;
+                myCumulate = 0.0f;
+                myResult = 0.0f;
+                myIndexB = -1;
+                myIndexD = -1;
+                for (k = 0; k < myType.Count; k++)
                 {
-                    if (myType[i] == 'D')
+                    if (myFoNo[k] == j && myType[k] == 'B')
                     {
-                        diff = (myDate[myIndex] - myDate[i]).Days;
-                        sum = sum + myAmmo[i];
-                        cumulate = cumulate + diff * myAmmo[i];
+                        myIndexB = k;
+                    }
+                    if (myFoNo[k] == j && myType[k] == 'D')
+                    {
+                        myIndexD = k;
                     }
                 }
-                result = (myAmmo[myIndex] - sum) * 365.0f / cumulate * 100.0f;
+                if (myIndexB < 0 || myIndexD < 0)
+                {
+                    myIndex = myFoNo.IndexOf(j);
+                    Console.WriteLine("There is no deposit or balance for " + myFond[myIndex]);
+                }
+                else
+                {
+                    for (k = 0; k < myType.Count; k++)
+                    {
+                        if (myFoNo[k] == j && myType[k] == 'D')
+                        {
+                            myDiff = (myDate[myIndexB] - myDate[k]).Days;
+                            mySum = mySum + myAmmo[k];
+                            myCumulate = myCumulate + myDiff * myAmmo[k];
+                        }
+                    }
+                    myResult = (myAmmo[myIndexB] - mySum) * 365.0f / myCumulate * 100.0f;
+                    myIndex = myFoNo.IndexOf(j);
+                    Console.WriteLine(myResult.ToString("n2") + " " + myFond[myIndex]);
+                }
             }
-            Console.WriteLine(result);
-            Console.ReadLine();
+        Console.ReadLine();
         }
     }
 }
